cfdbb4e58cd819346790fe091e2ba518918f1415
Lars de Bruijn <larsdebruijn12@gmail.com>
2015-03-26 16:01:11 +0100

    Fixed Globalization import

--- Source/Chronozoom.Entities/DocumentStorage.cs
+++ Source/Chronozoom.Entities/DocumentStorage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 // Add DocumentDB references
 using Microsoft.Azure.Documents;
