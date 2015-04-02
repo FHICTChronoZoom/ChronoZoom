94b2f9266d9e18da55eae5d38f1c75b842380f3e
Lars de Bruijn <larsdebruijn12@gmail.com>
2015-04-02 11:28:22 +0200

    Created new Timeline entity and improved documentation

--- Source/Chronozoom.Entities/rCollection.cs
+++ Source/Chronozoom.Entities/rCollection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,10 @@ namespace Chronozoom.Entities
     {
         public rCollection()
         {
-            this.Id = new Guid();
+            this.Id = Guid.NewGuid();
+            this.Default = false;
+            this.MembersAllowed = false;
+            this.PubliclySearchable = false;
         }
 
         /// <summary>
@@ -30,17 +34,25 @@ namespace Chronozoom.Entities
 
         /// <summary>
         /// Boolean which states if this is the default collection within a super collection.
-        /// Only one collection should be default within a super collection
+        /// Only one collection should be default within a super collection.
+        /// Default is false;
         /// </summary>
         public bool Default { get; set; }
 
         /// <summary>
-        /// Boolean to determine if this collection is open to edit for other members
+        /// Boolean to determine if this collection is open to edit for a list of members.
+        /// Default is false;
         /// </summary>
         public bool MembersAllowed { get; set; }
 
         /// <summary>
-        /// Boolena to determine if this collection is publically searchable
+        /// A list of users who have special rights to the collection, other than the collection owner.
+        /// </summary>
+        public virtual Collection<Member> Members { get; set; }
+
+        /// <summary>
+        /// Boolena to determine if this collection is publically searchable.
+        /// Default is false;
         /// </summary>
         public bool PubliclySearchable { get; set; }
 
@@ -48,6 +60,12 @@ namespace Chronozoom.Entities
         /// Title of the collection
         /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// URL-sanitized version of title, which will be used as part of a URL path.
+        /// Only a-z, 0-9 and hyphen are allowed. This should be unique per supercollection.
+        /// </summary>
+        public string Path { get; set; }
 
         /// <summary>
         /// Theme of the collection
@@ -58,5 +76,19 @@ namespace Chronozoom.Entities
         /// A list of timelines which are part of this collection
         /// </summary>
         public List<rTimeline> timelines { get; set; }
+
+        /// <summary>
+        /// The Collection.Theme text field is actually a compressed JSON object comprising of the following.
+        /// </summary>
+        public class Theme
+        {
+            public string backgroundUrl { get; set; }
+            public string backgroundColor { get; set; }
+            public string timelineColor { get; set; }
+            public string timelineStrokeStyle { get; set; }
+            public string infoDotfillColor { get; set; }
+            public string infoDotBorderColor { get; set; }
+            public Boolean kioskMode { get; set; }
+        }
     }
 }
---
+++ Source/Chronozoom.Entities/rTimeline.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Chronozoom.Entities
+{
+    class rTimeline
+    {
+        public rTimeline()
+        {
+            this.Id = Guid.NewGuid();
+            this.FromIsCirca = false;
+            this.ToIsCirca = false;
+        }
+
+        /// <summary>
+        /// Guid identifying the timeline
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Integer determining the depth of the timeline.
+        /// Used by the frontend to render
+        /// </summary>
+        public int Depth { get; set; }
+
+        /// <summary>
+        /// The title of the timeline
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The regime the timeline is part of
+        /// One of the hardcoded values within the application, somewhere.
+        /// </summary>
+        public string Regime { get; set; }
+
+        /// <summary>
+        /// Integer which determines where the timeline will start
+        /// </summary>
+        public decimal FromYear { get; set; }
+
+        /// <summary>
+        /// Integer which determines where the timeline will end
+        /// </summary>
+        public decimal ToYear { get; set; }
+
+        /// <summary>
+        /// No bloody clue.
+        /// </summary>
+        public decimal ForkNode { get; set; }
+
+        /// <summary>
+        /// The height of the timeline.
+        /// The question mark makes this field nullable.
+        /// </summary>
+        public decimal? Height { get; set; }
+
+        /// <summary>
+        /// Another Guid for the timeline. Sam knows why.
+        /// </summary>
+        public Guid TimeLineId { get; set; }
+
+        /// <summary>
+        /// The CollectionId which this timeline is part of
+        /// </summary>
+        public Guid CollectionId { get; set; }
+
+        /// <summary>
+        /// The number of content items contained in subtree under current timeline.
+        /// </summary>
+        public int SubtreeSize { get; set; }
+
+        /// <summary>
+        /// Boolean which determines if the FromYear field is an approximation.
+        /// Default is false
+        /// </summary>
+        public bool FromIsCirca { get; set; }
+
+        /// <summary>
+        /// Boolean which determines if the ToYear field is an approximation
+        /// Default is false
+        /// </summary>
+        public bool ToIsCirca { get; set; }
+
+        /// <summary>
+        /// The URL of the background image
+        /// </summary>
+        public string BackgroundUrl { get; set; }
+
+        /// <summary>
+        /// The Aspect ratio of the timeline
+        /// Aspect ratio = (width / height)
+        /// </summary>
+        public decimal? AspectRatio { get; set; }
+
+        /// <summary>
+        /// The child timelines belonging to this timeline
+        /// </summary>
+        public virtual Collection<rTimeline> ChildTimelines { get; set; }
+
+        /// <summary>
+        /// The exhibits belonging to this timeline
+        /// </summary>
+        public virtual Collection<Exhibit> Exhibits { get; set; }
+    }
+
+
+}
