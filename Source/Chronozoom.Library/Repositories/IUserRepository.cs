6c212de6de4bb3472ac1adca22fb35dce41f424f
Galvarino <galvarino_hofman@hotmail.com>
2015-05-01 14:56:27 +0200

    Added User in Model and IUserRepository in Repository

---
+++ Source/Chronozoom.Library/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Chronozoom.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronozoom.Library.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> FindByUsernameAsync(String username);
+    }
+}
