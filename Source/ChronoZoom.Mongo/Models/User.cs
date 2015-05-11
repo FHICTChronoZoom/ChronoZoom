3678c2e52681613b064b45ac1b2c2e64cc286dc5
Lars de Bruijn <larsdebruijn12@gmail.com>
2015-05-11 11:11:46 +0200

    Added User and Collection Models

---
+++ Source/ChronoZoom.Mongo/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronoZoom.Mongo.Models
+{
+    class User
+    {
+        public User() { }
+
+        /// <summary>
+        /// Unique identifier for the user
+        /// </summary>
+        [BsonId]
+        public ObjectId id { get; set; }
+
+        /// <summary>
+        /// Full name of the user
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Email address of the user
+        /// </summary>
+        public string email { get; set; }
+
+        /// <summary>
+        /// List of collections from the user.
+        /// </summary>
+        public List<Collection> collections { get; set; }
+    }
+}
