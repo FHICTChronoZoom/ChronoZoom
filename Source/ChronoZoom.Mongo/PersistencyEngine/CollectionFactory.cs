ae88c84456c7ff45e75d6113f29fe264fd17fb19
Max de Bruijn <max.de.bruijn@gmail.com>
2015-05-11 15:42:22 +0200

    Added Collection Factory

---
+++ Source/ChronoZoom.Mongo/PersistencyEngine/CollectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChronoZoom.Mongo.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ChronoZoom.Mongo.PersistencyEngine
+{
+    class CollectionFactory
+    {
+        public CollectionFactory() { }
+
+        public async Task<Collection> findById(ObjectId collectionId)
+        {
+            var collection = MongoFactory.database.GetCollection<Collection>("collection");
+            var chronoCollection = await collection.Find<Collection>(x => x.Id == collectionId).FirstOrDefaultAsync();
+
+            return chronoCollection;
+        }
+    }
+}
