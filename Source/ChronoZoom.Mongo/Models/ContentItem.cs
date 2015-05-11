3dd88cb7d4261417b984332af4ca309bf5319f56
Max de Bruijn <max.de.bruijn@gmail.com>
2015-05-11 13:03:46 +0200

    Added documentation to the contentItem and exhibit models

--- Source/ChronoZoom.Mongo/Models/ContentItem.cs
+++ Source/ChronoZoom.Mongo/Models/ContentItem.cs
@@ -11,49 +11,59 @@ namespace ChronoZoom.Mongo.Models
     {
 
         public ContentItem(){ }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int depth { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string title { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string caption { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int year { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public Media media { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string uri { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string attribution { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int order { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
+        public int depth { get; set; }
+
+        /// <summary>
+        /// Stores the title of the contentItem
+        /// </summary>
+        public string title { get; set; }
+
+        /// <summary>
+        /// Stores the caption of the contentItem (small description)
+        /// </summary>
+        public string caption { get; set; }
+
+        /// <summary>
+        /// Stores the year of the contentItem
+        /// </summary>
+        public int year { get; set; }
+
+        /// <summary>
+        /// Stores the mediatype and url of a contentItem
+        /// </summary>
+        public Media media { get; set; }
+
+        /// <summary>
+        /// Stores the link used to find this contentItem
+        /// </summary>
+        public string uri { get; set; }
+
+        /// <summary>
+        /// Stores the name of the person who curated the contentItem
+        /// </summary>
+        public string attribution { get; set; }
+
+        /// <summary>
+        /// Ordernumber in the exhibit
+        /// </summary>
+        public int order { get; set; }
+
+        /// <summary>
+        /// Inner class used to determine which kind of media type it is and where to find the source
+        /// </summary>
         internal class Media
         {
             public Media() { }
+
             /// <summary>
             /// 
             /// </summary>
             public string type { get; set; }
+
             /// <summary>
             /// 
             /// </summary>
