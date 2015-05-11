c3c2b59a6a571945f94041db0fbbba2354b44be9
Max de Bruijn <max.de.bruijn@gmail.com>
2015-05-11 11:43:25 +0200

    Added skeleton for the Exhibit and ContentItem models

---
+++ Source/ChronoZoom.Mongo/Models/ContentItem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronoZoom.Mongo.Models
+{
+ 
+    class ContentItem
+    {
+
+        public ContentItem(){ }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int depth { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string caption { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int year { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Media media { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string uri { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string attribution { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int order { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal class Media
+        {
+            public Media() { }
+            /// <summary>
+            /// 
+            /// </summary>
+            public string type { get; set; }
+            /// <summary>
+            /// 
+            /// </summary>
+            public string source { get; set; }
+        }
+        
+
+    }
+}
---
+++ Source/ChronoZoom.Mongo/Models/Exhibit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace ChronoZoom.Mongo.Models
+{
+
+    class Exhibit
+    {
+        public Exhibit() { }
+
+        [BsonId]
+        public ObjectId id { get; set; }
+        public int depth { get; set; }
+        public string title { get; set; }
+        public int year { get; set; }
+        public bool isCirca { get; set; }
+
+        public Updated updated { get; set; }
+        public List<ContentItem> contentItems { get; set; } 
+
+
+        internal class Updated
+        {
+            public Updated(){}
+
+            public ObjectId id { get; set; }
+            public string timestamp { get; set; }
+        }
+    }
+}
