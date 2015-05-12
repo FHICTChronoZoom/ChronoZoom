3134d057e0936a11d54909c6aff866f7cd7f10e9
Lars de Bruijn <larsdebruijn12@gmail.com>
2015-05-12 14:10:09 +0200

    Created findByEmail method for the user factory

--- Source/ChronoZoom.Mongo/PersistencyEngine/UserFactory.cs
+++ Source/ChronoZoom.Mongo/PersistencyEngine/UserFactory.cs
@@ -12,22 +12,18 @@ namespace ChronoZoom.Mongo.PersistencyEngine
     public class UserFactory
     {
 
-        public UserFactory() { 
-            
-        }
+        private UserFactory() { }
 
         /// <summary>
         /// Finds a user by his unique identifier
         /// </summary>
         /// <param name="userId">The unique identifier of the user</param>
-        /// <returns>The user object or a default</returns>
-        public async Task<User> findById(ObjectId userId) 
+        /// <returns>The user object or null</returns>
+        public static async Task<User> findById(ObjectId userId) 
         {
             var collection = MongoFactory.database.GetCollection<User>("user");
             var user = await collection.Find<User>(x => x.Id == userId).FirstOrDefaultAsync();
 
-            //var collections = CollectionFactory.findMultipleById(user.Collections);
-            //user.Collections = collections.Result;
             return user;
         }
 
@@ -35,11 +31,24 @@ namespace ChronoZoom.Mongo.PersistencyEngine
         /// Finds a user by his name
         /// </summary>
         /// <param name="name">The name of the user</param>
-        /// <returns>The user object or a default</returns>
-        public async Task<User> findByName(String name) 
+        /// <returns>The user object or null</returns>
+        public static async Task<User> findByName(String name) 
         {
             var collection = MongoFactory.database.GetCollection<User>("user");
             var user = await collection.Find<User>(x => x.Name == name).FirstOrDefaultAsync();
+
+            return user;
+        }
+
+        /// <summary>
+        /// Finds a user by his email
+        /// </summary>
+        /// <param name="email">The email of the user to be found</param>
+        /// <returns>The user object or null</returns>
+        public static async Task<User> findByEmail(String email)
+        {
+            var collection = MongoFactory.database.GetCollection<User>("user");
+            var user = await collection.Find<User>(x => x.Email == email).FirstOrDefaultAsync();
 
             return user;
         }
@@ -49,7 +58,7 @@ namespace ChronoZoom.Mongo.PersistencyEngine
         /// </summary>
         /// <param name="user"></param>
         /// <returns></returns>
-        public async Task<Boolean> create(User user) 
+        public static async Task<Boolean> create(User user) 
         {
             var collection = MongoFactory.database.GetCollection<User>("user");
             await collection.InsertOneAsync(user);
