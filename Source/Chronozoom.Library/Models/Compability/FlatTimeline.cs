ad951c52dd2d493f1028551c75b5c6bb7db7f1fa
Zef Ubachs <zef_ubachs@hotmail.com>
2015-05-18 09:24:58 +0200

    Added Service classes and a compability model for Timeline (uses in export and import).

---
+++ Source/Chronozoom.Library/Models/Compability/FlatTimeline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronozoom.Library.Models.Compability
+{
+    public class FlatTimeline
+    {
+        public Guid? ParentTimelineId { get; set; }
+        public Timeline Timeline { get; set; }
+        public List<Exhibit> Exhibits { get; set; }
+    }
+}
---
+++ Source/Chronozoom.Library/Services/ExportService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chronozoom.Library.Models;
+using Chronozoom.Library.Models.Compability;
+using Chronozoom.Library.Repositories;
+
+namespace Chronozoom.Library.Services
+{
+    public class ExportService
+    {
+        private ITimelineRepository timelineRepository;
+        private IExhibitRepository exhibitRepository;
+
+        public ExportService(ITimelineRepository timelineRepository, IExhibitRepository exhibitRepository)
+        {
+            if (timelineRepository == null) throw new ArgumentNullException("timelineRepository");
+            if (exhibitRepository == null) throw new ArgumentNullException("exhibitRepository");
+            this.timelineRepository = timelineRepository;
+            this.exhibitRepository = exhibitRepository;
+        }
+
+        public async Task<IEnumerable<FlatTimeline>> ExportTimeline(Guid topmostTimelineId)
+        {
+            var root = await timelineRepository.FindAsync(topmostTimelineId);
+            if (root == null)
+                throw new Exception("The timeline, \"" + topmostTimelineId.ToString() + "\", you want to export, does not exist.");
+
+            var timelines = new List<FlatTimeline>();
+            await IterateTimelineAsync(timelines, null, root);
+            return timelines;
+        }
+
+        public async Task<Exhibit> ExportExhibit(Guid exhibitId)
+        {
+            return await exhibitRepository.FindAsync(exhibitId);
+        }
+
+        private async Task IterateTimelineAsync(List<FlatTimeline> timelines, Guid? parentId, Timeline timeline)
+        {
+            var flatTimeline = new FlatTimeline
+            {
+                ParentTimelineId = parentId,
+                Timeline = timeline,
+            };
+            
+            var exhibits = await exhibitRepository.GetByTimelineAsync(timeline.Id);
+            flatTimeline.Exhibits = exhibits.ToList();
+
+            // Add parent before child, avoid having to call .Reverse() = performance gain?
+            timelines.Add(flatTimeline);
+            var children = await timelineRepository.GetByTimelineAsync(timeline.Id);
+            foreach (var child in children)
+            {
+                await IterateTimelineAsync(timelines, timeline.Id, child);
+            }
+        }
+    }
+}
