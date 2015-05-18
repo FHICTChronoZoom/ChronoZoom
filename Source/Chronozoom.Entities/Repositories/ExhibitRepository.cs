c317303b33fd40204e88fcfc06a2a051ee8a917b
Galvarino <galvarino_hofman@hotmail.com>
2015-05-18 13:01:21 +0200

    Timeline + Exhibit

---
+++ Source/Chronozoom.Entities/Repositories/ExhibitRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chronozoom.Library.Repositories;
+using System.Data.Entity;
+
+namespace Chronozoom.Entities.Repositories
+{
+    public class ExhibitRepository : IExhibitRepository
+    {
+        private Storage storage;
+
+        public ExhibitRepository(Storage storage)
+        {
+            this.storage = storage;
+        }
+
+        public async Task<IEnumerable<Library.Models.Exhibit>> GetByTimelineAsync(Guid timelineId)
+        {
+            var exhibit = await storage.Exhibits.Where(x => x.TimelineId);
+            return exhibit;
+        }
+
+        public async Task<Library.Models.Exhibit> FindAsync(Guid id)
+        {
+            var exhibit = await storage.Exhibits.FindAsync(id);
+            return ToLibraryExhibit(exhibit);
+        }
+
+        public async Task InsertAsync(Library.Models.Exhibit item)
+        {
+            var exhibit = ToEntityExhibit(item);
+            storage.Exhibits.Add(exhibit);
+            await storage.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Library.Models.Exhibit item)
+        {
+            var exhibit = await storage.Exhibits.FindAsync(item.Id);
+            
+            //Doubts
+            exhibit.ContentItems = (ContentItem) item.ContentItems;
+            exhibit.UpdatedBy = item.UpdatedBy;
+
+            exhibit.Depth = item.Depth;
+            exhibit.IsCirca = item.IsCirca;
+            exhibit.Title = item.Title;
+            exhibit.UpdatedTime = item.UpdatedTime;
+            exhibit.Year = item.Year;
+            await storage.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var exhibit = await storage.Exhibits.FindAsync(id);
+            storage.Exhibits.Remove(exhibit);
+            await storage.SaveChangesAsync();
+        }
+
+        private Library.Models.Exhibit ToLibraryExhibit(Exhibit exhibit)
+        {
+            return new Library.Models.Exhibit { Id = exhibit.Id, ContentItems = exhibit.ContentItems, Depth = exhibit.Depth, IsCirca = exhibit.IsCirca, Title = exhibit.Title, UpdatedTime = exhibit.UpdatedTime, UpdatedBy = exhibit.UpdatedBy, Year = exhibit.Year, TimelineId = exhibit.TimelineId };
+        }
+
+        private Exhibit ToEntityExhibit(Library.Models.Exhibit exhibit)
+        {
+            return new Exhibit { Id = exhibit.Id, ContentItems = exhibit.ContentItems, Depth = exhibit.Depth, IsCirca = exhibit.IsCirca, Title = exhibit.Title, UpdatedTime = exhibit.UpdatedTime, UpdatedBy = exhibit.UpdatedBy, Year = exhibit.Year, TimelineId = exhibit.TimelineId };
+        }
+    }
+}
---
+++ Source/Chronozoom.Entities/Repositories/TimeLineRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chronozoom.Library.Repositories;
+using System.Data.Entity;
+
+namespace Chronozoom.Entities.Repositories
+{
+    public class TimeLineRepository : ITimelineRepository
+    {
+        private Storage storage;
+
+        public TimeLineRepository(Storage storage)
+        {
+            this.storage = storage;
+        }
+
+        public async Task<IEnumerable<Library.Models.Timeline>> GetByCollectionAsync(Guid collectionId)
+        {
+            
+        }
+
+        public async Task<IEnumerable<Library.Models.Timeline>> GetByTimelineAsync(Guid timelineId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Library.Models.Timeline> FindAsync(Guid id)
+        {
+            var timeline = await storage.Timelines.FindAsync(id);
+            return ToLibraryTimeLine(timeline);
+        }
+
+        public async Task InsertAsync(Library.Models.Timeline item)
+        {
+            var timeline = ToEntityTimeline(item);
+            storage.Timelines.Add(timeline);
+            await storage.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Library.Models.Timeline item)
+        {
+            var timeline = await storage.Exhibits.FindAsync(item.Id);
+
+            timeline.Title = item.Title;
+            timeline.IsCirca = item.ToIsCirca;
+
+            timeline.Regime = item.Regime;
+            timeline.FromYear = item.FromYear;
+            timeline.FromIsCirca = item.FromIsCirca;
+            timeline.ToYear = item.ToYear;
+            timeline.ToIsCirca = item.ToIsCirca;
+            timeline.Height = item.Height;
+            timeline.BackgroundUrl = item.BackgroundUrl;
+            timeline.AspectRatio = item.AspectRatio;
+
+            await storage.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var timeline = await storage.Timelines.FindAsync(id);
+            storage.Timelines.Remove(timeline);
+            await storage.SaveChangesAsync();
+        }
+
+        private Library.Models.Timeline ToLibraryTimeLine(Timeline timeline)
+        {
+            // NO PARENTID
+            return new Library.Models.Timeline { Id = timeline.Id, Title = timeline.Title, Regime = timeline.Regime, FromYear = timeline.FromYear, FromIsCirca = timeline.FromIsCirca, ToYear = timeline.ToYear, ToIsCirca = timeline.ToIsCirca, Height = timeline.Height, BackgroundUrl = timeline.BackgroundUrl, AspectRatio = timeline.AspectRatio };
+        }
+
+        private Timeline ToEntityTimeline(Library.Models.Timeline timeline)
+        {
+            return new Timeline { Id = timeline.Id, Title = timeline.Title, Regime = timeline.Regime, FromYear = timeline.FromYear, FromIsCirca = timeline.FromIsCirca, ToYear = timeline.ToYear, ToIsCirca = timeline.ToIsCirca, Height = timeline.Height, BackgroundUrl = timeline.BackgroundUrl, AspectRatio = timeline.AspectRatio };
+        }
+    
+    }
+}
