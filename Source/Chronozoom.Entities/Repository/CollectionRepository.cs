9d359636fed96b2d88417564055936c8f09cb890
Galvarino <galvarino_hofman@hotmail.com>
2015-05-18 09:23:36 +0200

    Repositories and Tour.class

---
+++ Source/Chronozoom.Entities/Repository/CollectionRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chronozoom.Library.Repositories;
+using System.Data.Entity;
+
+namespace Chronozoom.Entities.Repository
+{
+    class CollectionRepository : ICollectionRepository
+    {
+        private Storage storage;
+
+        public CollectionRepository(Storage storage)
+        {
+            this.storage = storage;
+        }
+
+        public async Task<IEnumerable<Library.Models.Collection>> GetPublicCollectionsAsync()
+        {
+            var collection = await storage.Collections.Where(x => x.PubliclySearchable).ToListAsync();
+            return collection.ConvertAll(x => new Library.Models.Collection { Id = x.Id, Default = x.Default, IsPublicSearchable = x.PubliclySearchable, Theme = x.Theme, Title = x.Title });
+        }
+
+        public async Task<IEnumerable<Library.Models.Collection>> GetByUserAsync(Guid userId)
+        {
+            var collection = await storage.Collections.Where(x => x.User.Id == userId).ToListAsync();
+            return collection.ConvertAll(x => new Library.Models.Collection { Id = x.Id, Default = x.Default, IsPublicSearchable = x.PubliclySearchable, Title = x.Title, Theme = x.Theme });
+        }
+
+        public async Task<Library.Models.Collection> GetUserDefaultAsync(Guid userId)
+        {
+            var collection = await storage.Collections.FirstOrDefaultAsync(x => x.User.Id == userId && x.Default);
+            if (collection == null ) {
+                // TODO: ensure personal collection (Make a new default collection that is empty)
+                return null;
+            }
+            else
+                return ToLibraryCollection(collection);
+        }
+
+        public async Task<bool> IsMemberAsync(Guid collectionId, Guid userId)
+        {
+            var collection = await storage.Collections.FindAsync(collectionId);
+            if (collection.User.Id == userId || collection.Members.Any(x => x.User.Id == userId))
+                return true;
+            else
+                return false;
+        }
+
+        public async Task<Library.Models.Collection> FindAsync(Guid id)
+        {
+            var collection = await storage.Collections.FindAsync(id);
+            return ToLibraryCollection(collection);
+        }
+
+        public async Task InsertAsync(Library.Models.Collection item)
+        {
+            var collection = ToEntityCollection(item);
+            storage.Collections.Add(collection);
+            await storage.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Library.Models.Collection item)
+        {
+            var collection = await storage.Collections.FindAsync(item.Id);
+            collection.Default = item.Default;
+            collection.PubliclySearchable = item.IsPublicSearchable;
+            collection.Theme = item.Theme;
+            collection.Title = item.Title;
+            await storage.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var collection = await storage.Collections.FindAsync(id);
+            storage.Collections.Remove(collection);
+            await storage.SaveChangesAsync();
+        }
+
+        private Library.Models.Collection ToLibraryCollection(Collection col)
+        {
+            return new Library.Models.Collection { Id = col.Id, Default = col.Default, IsPublicSearchable = col.PubliclySearchable, Theme = col.Theme, Title = col.Title };    
+        }
+
+        private Collection ToEntityCollection(Library.Models.Collection col)
+        {
+            return new Collection { Id = col.Id, Default = col.Default, PubliclySearchable = col.IsPublicSearchable, Theme = col.Theme, Title = col.Title };
+        }
+    }
+}
---
+++ Source/Chronozoom.Entities/Repository/TourRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chronozoom.Library.Repositories;
+using System.Data.Entity;
+
+namespace Chronozoom.Entities.Repository
+{
+    class TourRepository : ITourRepository
+    {
+        private Storage storage;
+
+        public TourRepository(Storage storage)
+        {
+            this.storage = storage;
+        }
+
+        public async Task<Library.Models.Tour> FindAsync(Guid id)
+        {
+            var tour = await storage.Tours.FindAsync(id);
+            return ToLibraryTour(tour);
+        }
+
+        public async Task InsertAsync(Library.Models.Tour item)
+        {
+            var tour = ToEntityTour(item);
+            storage.Tours.Add(tour);
+            await storage.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Library.Models.Tour item)
+        {
+            var tour = await storage.Tours.FindAsync(item.Id);
+            //TODO: Properties inserten
+            await storage.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var tour = await storage.Tours.FindAsync(id);
+            storage.Tours.Remove(tour);
+            await storage.SaveChangesAsync();
+        }
+
+        private Library.Models.Tour ToLibraryTour(Tour tour)
+        {
+            return new Library.Models.Tour { Id = tour.Id, AudioBlobUrl = tour.AudioBlobUrl, Category = tour.Category, Description = tour.Description, Name = tour.Name, Sequence = (int)tour.Sequence, UniqueId = tour.UniqueId };
+        }
+
+        private Tour ToEntityTour(Library.Models.Tour tour)
+        {
+            return new Tour { Id = tour.Id, AudioBlobUrl = tour.AudioBlobUrl, Category = tour.Category, Description = tour.Description, Name = tour.Name, Sequence = tour.Sequence, UniqueId = tour.UniqueId };
+        }
+    }
+}
