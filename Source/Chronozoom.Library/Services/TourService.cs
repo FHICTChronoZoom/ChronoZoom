51642eac494728c465b66efd4f791c753f1b9517
Rens Groothuijsen <r.groothuijsen@student.fontys.nl>
2015-06-05 14:00:17 +0200

    Added GetTour and required Controller/Service

---
+++ Source/Chronozoom.Library/Services/TourService.cs
@@ -0,0 +1,27 @@
+﻿using Chronozoom.Business.Models;
+using Chronozoom.Business.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronozoom.Business.Services
+{
+    
+    class TourService
+    {
+        private ITourRepository tourRepository;
+
+        public TourService(ITourRepository tourRepository)
+        {
+            if (tourRepository == null) throw new ArgumentNullException("tourRepository");
+            this.tourRepository = tourRepository;
+        }
+
+        public Task<Tour> GetTourAsync(Guid id)
+        {
+            return tourRepository.FindByIdAsync(id);
+        }
+    }
+}
---
+++ Source/Chronozoom.UI/Controllers/Api/TourController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Chronozoom.Business.Services;
+using System.Threading.Tasks;
+
+namespace Chronozoom.UI.Controllers.Api
+{
+    public class TourController : ApiController
+    {
+        private TourService tourService;
+
+        public TourController(TourService tourService)
+        {
+            this.tourService = tourService;
+        }
+
+        [HttpPut]
+        [Route("~/api/v2/tour/{id:Guid}")]
+        public async Task<IHttpActionResult> GetTour(Guid id)
+        {
+            try
+            {
+                await tourService.GetTourAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+
+
+}
