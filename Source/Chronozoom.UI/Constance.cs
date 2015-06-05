973681813028cac01521af667177efcd8d9f66cd
Galvarino <galvarino_hofman@hotmail.com>
2015-06-05 10:20:50 +0200

    new constance class; place here all your static variables

---
+++ Source/Chronozoom.UI/Constance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chronozoom.UI
+{
+    public class Constance
+    {
+        public static readonly string _defaultSuperCollectionName = "DefaultSuperCollection";
+    }
+}
