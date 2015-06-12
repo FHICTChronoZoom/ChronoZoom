0a2ee83269e8f552abe213a77201a07a808cc088
Lars de Bruijn <larsdebruijn12@gmail.com>
2015-06-12 11:14:36 +0200

    Added exhibit service

---
+++ Source/Chronozoom.Library/Services/ExhibitService.cs
@@ -0,0 +1,25 @@
+﻿using Chronozoom.Business.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronozoom.Business.Services
+{
+    class ExhibitService
+    {
+        private IExhibitRepository exhibitRepository;
+
+        public ExhibitService(IExhibitRepository exhibitRepository)
+        {
+            if (exhibitRepository == null) throw new ArgumentNullException("timelineRepository");
+            this.exhibitRepository = exhibitRepository;
+        }
+
+        public async Task<IEnumerable<Timeline>> GetRootTimelines(Guid collectionId)
+        {
+            return await timelineRepository.GetRootTimelines(collectionId);
+        }
+    }
+}
