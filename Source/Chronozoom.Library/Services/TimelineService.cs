52f4e455c4df4c137c9866e98f93cb943f7aeb46
Lars de Bruijn <larsdebruijn12@gmail.com>
2015-06-12 11:10:13 +0200

    Overloaded method for deleting a timeline

--- Source/Chronozoom.Library/Services/TimelineService.cs
+++ Source/Chronozoom.Library/Services/TimelineService.cs
@@ -24,7 +24,23 @@ namespace Chronozoom.Business.Services
             return await timelineRepository.GetRootTimelines(collectionId);
         }
 
+        /// <summary>
+        /// Deletes a timeline with the specified Id
+        /// </summary>
+        /// <param name="superCollectionName">The superCollection name/User name whom the timeline belongs to</param>
+        /// <param name="timelineRequest">The timeline to be deleted</param>
         public async void DeleteTimeline(string superCollectionName, Timeline timelineRequest) {
+            await timelineRepository.DeleteAsync(timelineRequest.Id);
+        }
+
+        /// <summary>
+        /// Overloaded method for deleting a timeline with the specified Id
+        /// </summary>
+        /// <param name="superCollectionName">The superCollection name/User name whom the timeline belongs to</param>
+        /// <param name="collectionName">The name of the collection of which the timeline should be deleted (doesn't do anything for now)</param>
+        /// <param name="timelineRequest">The timeline to be deleted</param>
+        public async void DeleteTimeline(string superCollectionName, string collectionName, Timeline timelineRequest)
+        {
             await timelineRepository.DeleteAsync(timelineRequest.Id);
         }
     }
