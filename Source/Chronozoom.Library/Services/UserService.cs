4550444d59cabcc762af23084bcabd9e6db16b22
Lars de Bruijn <larsdebruijn12@gmail.com>
2015-06-12 11:41:57 +0200

    Moved the IsMember collection functionality to the collectionService

--- Source/Chronozoom.Library/Services/UserService.cs
+++ Source/Chronozoom.Library/Services/UserService.cs
@@ -50,20 +50,5 @@ namespace Chronozoom.Business.Services
 
             await userRepository.DeleteAsync(user.Id);
         }
-
-        /// <summary>
-        /// Checks if a user is a member of the collection and therefore has member priviliges
-        /// 
-        /// WARNING!! This function has too little parameters to actually function. 
-        /// Because of that I hardcoded the user Id parameter.
-        /// Also, the collectionId should be delivered as a Guid in stead of a string.
-        /// 
-        /// </summary>
-        /// <param name="collectionId">The collection to check membership for</param>
-        /// <returns>Boolean - True if the user has membership priviliges, otherwise false</returns>
-        public async Task<bool> UserIsMember(string collectionId)
-        {
-            return await collectionRepository.IsMemberAsync(new Guid(collectionId), new Guid());
-        }
     }
 }
