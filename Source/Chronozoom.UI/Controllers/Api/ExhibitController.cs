7d9e519321164517ca1a0fddda73c97b34532a5e
Rik-van-de-Laar <sandscorpion@msn.com>
2015-06-19 13:28:53 +0200

    GetExhibitLastUpdate

---
+++ Source/Chronozoom.UI/Controllers/Api/ExhibitController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Chronozoom.Business.Services;
+using System.Threading.Tasks;
+
+namespace Chronozoom.UI.Controllers.Api
+{
+    public class ExhibitController : ApiController
+    {
+        private ExhibitService exhibitService;
+
+        [HttpGet]
+        [Route("~/api/v2/exhibit/{exhibitId}/lastupdate")]
+        public async Task<IHttpActionResult> GetExhibitLastUpdate(string exhibitId)
+        {
+            var lastUpdated = await exhibitService.GetExhibitLastUpdate(exhibitId);
+            return Ok(lastUpdated);
+        }
+    }
+}
