88c5296148fe85984ef7876456d5533b5b5c0c59
Zef Ubachs <zef_ubachs@hotmail.com>
2015-06-25 19:55:21 +0200

    Added comments to UnityDependencyResolver.

--- Source/Chronozoom.UI/UnityDependencyResolver.cs
+++ Source/Chronozoom.UI/UnityDependencyResolver.cs
@@ -1,27 +1,40 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Http.Dependencies;
-using Microsoft.Practices.Unity;
 
 namespace Chronozoom.UI
 {
+    /// <summary>
+    /// A <see cref="System.Web.Http.Dependencies.IDependencyResolver"/> implementation for Microsoft's Unity dependency container.
+    /// </summary>
     public class UnityDependencyResolver : IDependencyResolver
     {
         private IUnityContainer unityContainer;
 
+        /// <summary>
+        /// Create a new instance of <see cref="Chronozoom.UI.UnityDependencyResolver"/>.
+        /// </summary>
+        /// <param name="unityContainer">The <see cref="Microsoft.Practices.Unity.IUnityContainer"/> that will be used to resolve types on runtime.</param>
         public UnityDependencyResolver(IUnityContainer unityContainer)
         {
             this.unityContainer = unityContainer;
         }
 
+        /// <summary>
+        /// Starts a resolution scope.
+        /// </summary>
+        /// <returns>The dependency scope.</returns>
         public IDependencyScope BeginScope()
         {
             return new UnityDependencyResolver(unityContainer.CreateChildContainer());
         }
 
+        /// <summary>
+        /// Retrieves a service from the scope.
+        /// </summary>
+        /// <param name="serviceType">The service to be retrieved.</param>
+        /// <returns>The retrieved service.</returns>
         public object GetService(Type serviceType)
         {
             try
@@ -34,6 +47,11 @@ namespace Chronozoom.UI
             }
         }
 
+        /// <summary>
+        /// Retrieves a collection of services from the scope.
+        /// </summary>
+        /// <param name="serviceType">The collection of services to be retrieved.</param>
+        /// <returns>The retrieved collection of services.</returns>
         public IEnumerable<object> GetServices(Type serviceType)
         {
             try
@@ -49,18 +67,26 @@ namespace Chronozoom.UI
         #region IDisposable Support
         private bool disposedValue = false;
 
-        protected virtual void Dispose(bool disposing)
+        /// <summary>
+        /// Override this method to dispose of additional resources.
+        /// </summary>
+        /// <param name="managed">True if Dispose is called, false if called by deconstructor.</param>
+        protected virtual void Dispose(bool managed)
         {
             if (!disposedValue)
             {
                 disposedValue = true;
-                if (disposing)
+                if (managed)
                 {
                     unityContainer.Dispose();
                 }
             }
         }
-        
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting
+        /// unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
